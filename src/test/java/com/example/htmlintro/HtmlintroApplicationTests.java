17f2ec65d603667cb454762e53e461cf5b1e6798
Ella Frost <ded_19@hotmail.com>
2022-09-16 13:25:57 +0200

    første

---
+++ src/test/java/com/example/htmlintro/HtmlintroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.htmlintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlintroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
